0edb921d2b3d6133cff62ba42098b1236f97b58f
haozi3 <xhy2654317349@126.com>
2024-03-28 10:49:43 +0800

    1.01.pcc

---
+++ 1.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	scanf("%d",&a);
+	while(a>1&&a%2==0)
+	{
+		a/=2;
+		
+		
+	}
+	if(a==1){
+			printf("%s","是2的幂函数");
+		}
+	else
+	{
+		printf("不是密函苏");
+	}
+	return 0;
+	
+ } 
